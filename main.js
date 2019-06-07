c8d19794d901c8724b2a35c8ad0ab2f778246657
Mohammed Asker <zetoanime@gmail.com>
2019-06-07 15:15:36 +0300

    added files

---
+++ main.js
@@ -0,0 +1,20 @@
+// user inputs the numbers
+ 
+ /*user enters the number
+ the numbers will appear on the display
+ */
+
+// calculate the numbers
+/*operate the calulation starts here
+number plus number equals number added
+number minus number equals number subtracted
+number times number equals number multipled
+number divided by number equals number divided
+
+the calculated number displays the result*/
+
+// make an error if user type incorrectly
+
+/*if the inputs is not correct
+produce error warning
+display "Invalid number"*/
