b942a3c746d486a5b9d26878bd7ac5930f955681
Mohammed Asker <zetoanime@gmail.com>
2019-06-10 12:03:45 +0300

    changed files

--- main.js
+++ main.js
@@ -1,22 +1,18 @@
 // user inputs the numbers
- 
- /*user enters the number
- the numbers will appear on the display
- */
- 
 function number(value) {
-document.form1.result.value +=value;
+    document.form1.result.value +=value;
+}
+
+// clear the display
+function cle(value) {
+	document.form1.result.value = value;
 }
  
-
 // calculate the numbers
-/*operate the calulation starts here
-number plus number equals number added
-number minus number equals number subtracted
-number times number equals number multipled
-number divided by number equals number divided
-
-the calculated number displays the result*/
+function evalua() {
+	document.form1.result.value = eval(
+	document.form1.result.value)
+}
 
 // make an error if user type incorrectly
 
