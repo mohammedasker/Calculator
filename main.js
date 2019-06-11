f0d267c3c2b1cddd0fc116246a1b526dfe0e1483
Mohammed Asker <zetoanime@gmail.com>
2019-06-11 11:11:25 +0300

    fixed CSS and changed display, background, & buttons colors

--- main.js
+++ main.js
@@ -1,21 +1,31 @@
-// user inputs the numbers
+// User inputs the numbers
 function number(value) {
     document.form1.result.value +=value;
 }
 
-// clear the display
+// Clear the display
 function cle(value) {
 	document.form1.result.value = value;
 }
  
-// calculate the numbers
+// Calculate the numbers
 function evalua() {
 	document.form1.result.value = eval(
 	document.form1.result.value)
 }
 
-// make an error if user type incorrectly
-
+// Make an error if user input incorrectly
 /*if the inputs is not correct
 produce error warning
 display "Invalid number"*/
+
+
+if (form1.result.value != value.result.value) {
+	//return ('Syntax_Error')
+	console.log('not typed correctly');
+}
+
+// Set digit input limits up to 15
+
+// Scale down the dight size when it exceeds 13 digits
+
